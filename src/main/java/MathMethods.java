33480b3eebfd8f22acb9ca3412c3e18d8833c5ab
alkekkonen <alexander.kekkonen@gmail.com>
2018-03-22 10:17:38 +0300

    Unit tests added

--- src/main/java/MathMethods.java
+++ src/main/java/MathMethods.java
@@ -1,8 +1,15 @@
 public class MathMethods {
 
     public int twoNumbersSum(int a, int b){
-        int sum = a + b;
-        return sum;
+        return a + b;
+    }
+
+    public int twoNumbersMultiply(int a, int b){
+        return a * b;
+    }
+
+    public int twoNumbersDivide(int firstNum, int secondNum){
+        return firstNum / secondNum;
     }
 
 }
--- src/test/java/TestMathMethods.java
+++ src/test/java/TestMathMethods.java
@@ -1,2 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMathMethods {
+
+    MathMethods mm = new MathMethods();
+
+    @Test
+    public void testTwoNumbersSumPositive(){
+        Assert.assertEquals(16,  mm.twoNumbersSum(6, 10));
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testTwoNumbersSumNegative(){
+        Assert.assertEquals(17,  mm.twoNumbersSum(10, 6));
+    }
+
+    @Test
+    public void testTwoNumbersMultiplyPositive(){
+        Assert.assertEquals(42,  mm.twoNumbersMultiply(7, 6));
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testTwoNumbersMultiplyNegative(){
+        Assert.assertEquals(26,  mm.twoNumbersMultiply(7, 6));
+    }
+
+
 }
