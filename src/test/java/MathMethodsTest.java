769f3aa4f7254229a1423cc42137f885787290d9
alkekkonen <alexander.kekkonen@gmail.com>
2018-03-23 09:27:45 +0300

    log4j test logging settings added

--- src/test/java/MathMethodsTest.java
+++ src/test/java/MathMethodsTest.java
@@ -13,24 +13,26 @@ public class MathMethodsTest {
 
     @Test
     public void testTwoNumbersSumPositive(){
-        log.debug("Start processing");
+        log.debug("Start processing testTwoNumbersSumPositive with DEBUG");
         Assert.assertEquals(16,  mm.twoNumbersSum(6, 10));
     }
 
     @Test(expected = AssertionError.class)
     public void testTwoNumbersSumNegative(){
+        log.warn("Start processing testTwoNumbersSumNegative with WARN");
         Assert.assertEquals(17,  mm.twoNumbersSum(10, 6));
     }
 
     @Test
     public void testTwoNumbersMultiplyPositive(){
+        log.info("Start processing testTwoNumbersMultiplyPositive with INFO");
         Assert.assertEquals(42,  mm.twoNumbersMultiply(7, 6));
     }
 
-    @Test(expected = AssertionError.class)
+    @Test (expected = AssertionError.class)
     public void testTwoNumbersMultiplyNegative(){
+        log.error("Start processing testTwoNumbersMultiplyNegative with ERROR");
         Assert.assertEquals(26,  mm.twoNumbersMultiply(7, 6));
     }
-
 
 }
